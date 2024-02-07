fa301caad20830188f6b8d364479d750bff71856
Md Nawazish Alam <nawazish0230@gmail.com>
2024-02-07 20:06:36 +0530

    fix: undo api changes

--- src/redux/home/action.ts
+++ src/redux/home/action.ts
@@ -1,14 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-// import responseData from './data.json'
 import { formatValueFromProductData } from "utils";
 
 export const fetchHomeData = createAsyncThunk('data/fetchHomeData', async () => {
   try {
-    // const response = await fetch('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory');
-    // const responseData = await response.json();
+    const response = await fetch('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory');
+    const responseData = await response.json();
 
-    // const updatedData = formatValueFromProductData(responseData)
-    return {};
+    const updatedData = formatValueFromProductData(responseData)
+    return updatedData;
   } catch (error) {
     console.log("api error: " + error)
   }
